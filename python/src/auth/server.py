0261eaae6a452de9d7a625a779ea19c473c77404
Daniel Wong <wongyuheng2003@gmail.com>
2024-01-02 17:06:26 +0800

    Edited SQL script and authorization service in server code

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -5,6 +5,7 @@ from flask_mysqldb import MySQL
 server = Flask(__name__)
 mysql = MySQL(server)
 
+
 ### CONFIG
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
@@ -17,8 +18,64 @@ server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 # Login route
 @server.route("/login", methods=["POST"])
 def login():
-    auth = request.authorization
+    auth = request.authorization  # --> "Authorization: Basic" header
     if not auth:
         return "[!] Missing credentials", 401
 
     # Check database for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s",
+        (auth.username, auth.password),
+    )
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "[!] Invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "[!] Invalid credentials", 401
+
+
+# JWT validation route
+@server.route("/validate", method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "[!] Missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "[!] Unauthorized", 403
+
+    return decoded, 200
+
+
+# JWT Auth Signing Function
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz.datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+if __name__ == "__main__":
+    server.run(port=5000, host="0.0.0.0")
